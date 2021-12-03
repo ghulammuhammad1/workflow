fd008bcfa9bbbf3931bb00d8becec4d047baa1c1
ghulammuhammad1 <ghulammuhammad.cs17@iba-suk.edu.pk>
2021-12-03 12:19:41 +0500

    S

--- hello.c
+++ hello.c
@@ -1,3 +1,4 @@
+//abc
 #include <linux/init.h> // Macros used to mark up functions e.g., __init __exit
 #include <linux/module.h> // Core header for loading LKMs into the kernel
 #include <linux/kernel.h> // Contains types, macros, functions for the kernel
