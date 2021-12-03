9d058b635e6da66c49eda08c6e8d94f26a18419f
ghulammuhammad1 <ghulammuhammad.cs17@iba-suk.edu.pk>
2021-12-03 17:17:13 +0500

    cde added

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 //abc
+//cde
 #include <linux/init.h> // Macros used to mark up functions e.g., __init __exit
 #include <linux/module.h> // Core header for loading LKMs into the kernel
 #include <linux/kernel.h> // Contains types, macros, functions for the kernel
